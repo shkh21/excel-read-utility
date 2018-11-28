2059a9bd21f8e427c5a032e78586d11a1954fe61
shkh21 <36784049+shkh21@users.noreply.github.com>
2018-11-28 23:17:40 +0530

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,7 +1,7 @@
 package utilities;
 
 public class Constants {
-	  public static final String URL = "http://www.store.demoqa.com";
-      public static final String File_Path = "C:\\Users\\Shubham\\workspace\\ApachePOI\\src\\utilities\\";
+	  public static final String URL = "http://www.anyweburl.com";
+      public static final String File_Path = "C:\\Users\\";
       public static final String File_Name = "ReadExcelData.xlsx";
 }
