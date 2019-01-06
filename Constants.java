6813125c8b92150d5aae2facce5f699fdbd7b9e7
shkh21 <36784049+shkh21@users.noreply.github.com>
2019-01-06 19:24:47 +0530

    Delete Constants.java

--- Constants.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class Constants {
-	  public static final String URL = "http://www.anyweburl.com";
-      public static final String File_Path = "C:\\Users\\";
-      public static final String File_Name = "ReadExcelData.xlsx";
-}
